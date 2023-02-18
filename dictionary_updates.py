382608f616a6f701e8e699dfa18a13678307776f
shahjaidev <shahjaidev99@gmail.com>
2023-02-18 18:57:18 +0000

    sparsevectorupdates

--- dictionary_updates.py
+++ dictionary_updates.py
@@ -1,25 +1,43 @@
 #TFIDF Dictionary Updates
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class SparseVectorUpdates:
     def __init__(self, dictionary):
-        self.term_frequency_dict = defaultdict(int)
+        self.all_relevant_doc_ids = set()
+        self.all_non_relevant_doc_ids = set()
+        self.relevant_term_frequency_dict = defaultdict(int)
+        self.non_relevant_term_frequency_dict = defaultdict(int)
         self.document_frequency_dict = defaultdict(int)
-        self.worf_tfidf_scores_dict = defaultdict(float)
+        self.word_tfidf_scores_dict = defaultdict(float)
 
+    def update_all_relevant_doc_ids(self, docdict):
+        for doc_id, doc_entry in docdict.items():
+            self.all_relevant_doc_ids.add(doc_id)
     
-    def update_term_frequency_dict(self, docdict):
+    def update_all_non_relevant_doc_ids(self, docdict):
+        for doc_id, doc_entry in docdict.items():
+            self.all_non_relevant_doc_ids.add(doc_id)
+    
+    def update_relevant_term_frequency_dict(self, docdict):
         for doc_id, doc_entry in docdict.items():
             processed_snippet = doc_entry['processed_snippet']
             for word in processed_snippet:
-                self.term_frequency_dict[(word, doc_id)] += 1
+                self.relevant_term_frequency_dict[(word, doc_id)] += 1
+    
+    def update_non_relevant_term_frequency_dict(self, docdict):
+        for doc_id, doc_entry in docdict.items():
+            processed_snippet = doc_entry['processed_snippet']
+            for word in processed_snippet:
+                self.non_relevant_term_frequency_dict[(word, doc_id)] += 1
     
     def update_document_frequency_dict(self, docdict):
         for doc_id, doc_entry in docdict.items():
             processed_snippet = doc_entry['processed_snippet']
-            for word in processed_snippet:
+            word_freq_in_snippet = Counter(processed_snippet)
+            for word in word_freq_in_snippet.keys():
                 self.document_frequency_dict[word] += 1
+            
 
 
 #git command to push to a branch
